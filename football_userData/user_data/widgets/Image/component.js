7beadec17c131992e59865134358abeed48b8dc9
Badger & Combes <95646286+BadgerMCR@users.noreply.github.com>
2023-01-30 11:27:41 +0000

    Delete football_userData/user_data directory

--- football_userData/user_data/widgets/Image/component.js
+++
@@ -1,8 +0,0 @@
-// Receives the widget, widgetModule and entries as props.
-export default {
-	computed: {
-		imageUrl: function() {
-			return "/mediafiles/" + this.widget.props.image
-		}
-	}
-}
--- football_userData/user_data/widgets/Video/component.js
+++
@@ -1,8 +0,0 @@
-// Receives the widget, widgetModule and entries as props.
-export default {
-	computed: {
-		videoUrl: function() {
-			return "/mediafiles/" + this.widget.props.video
-		}
-	}
-}
